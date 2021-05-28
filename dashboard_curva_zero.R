32806de26ea533e5dcc2bed51cd23ca9ad79e9ba
Felipe Simplício Ferreira <felipe.ferreira@ipea.gov.br>
2021-05-28 15:51:03 -0300

    Adição de personalização do gráfico
    Mudanças de estilo e de posição dos elementos dos gráficos

--- dashboard_curva_zero.R
+++ dashboard_curva_zero.R
@@ -8,7 +8,12 @@ library(rio)
 library(openxlsx)
 #library(shiny)
 #devtools::install_github('bbc/bbplot')
-#library(bbplot)
+library(bbplot)
+library(ggrepel)
+library(scales)
+
+##Definindo caminho
+setwd("D:/Documentos/GitHub/dashboard_curva_zero")
 
 
 
@@ -54,8 +59,14 @@ usuario_data <- function(){
   
   dado_graf <- get(resposta_data) %>% select(anos, resposta_dados) %>% rename(!!resposta_data := resposta_dados)
 
-  graf <- ggplot() + 
-    geom_line(data = dado_graf, aes(x = anos, y = get(!!resposta_data)))
+  graf <- ggplot(dado_graf, aes(x = anos, y = get(!!resposta_data), label = sprintf("%0.2f", round(dado_graf[,2],2)))) + 
+    geom_line() + bbc_style() + geom_label_repel() + 
+    scale_x_continuous(breaks = round(seq(min(dado_graf$anos), max(dado_graf$anos), by = 0.5),1)) + 
+    scale_y_continuous(breaks = round(seq(min(dado_graf[,2]), max(dado_graf[,2]), by = 0.5),1)) + 
+    theme(axis.text.x=element_text(angle=90, hjust=1)) + 
+    labs(title = paste("Curva zero", resposta_data, sep = " "), subtitle = resposta_dados,
+         caption = "Fonte: Ambima")
+  
   show(graf)
   
   for (i in 1:length(datas_tratadas)){
@@ -70,8 +81,13 @@ usuario_data <- function(){
     
     dado_graf_mult <- pivot_longer(dado_graf, -1)
     
-    graf <- graf + 
-      geom_line(data = dado_graf_mult, aes(x = anos, y = value, colour = name))
+    graf <- ggplot(dado_graf_mult, aes(x = anos, y = value, colour = name, label = sprintf("%0.2f", round(value,2)))) +
+      geom_line() + bbc_style() + geom_label_repel() + 
+      scale_x_continuous(breaks = round(seq(min(dado_graf_mult$anos), max(dado_graf_mult$anos), by = 0.5),1)) + 
+      scale_y_continuous(breaks = round(seq(min(dado_graf_mult$value), max(dado_graf_mult$value), by = 0.5),1)) + 
+      theme(axis.text.x=element_text(angle=90, hjust=1), legend.position = "bottom") + 
+      labs(title = "Curva zero", subtitle = resposta_dados,
+           caption = "Fonte: Ambima")
     show(graf)
   }
 }
