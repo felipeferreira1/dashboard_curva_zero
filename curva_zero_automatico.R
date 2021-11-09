0f00f11a7dd6809e069039d69ccb998af4426b0c
Felipe Simplício Ferreira <felipe.ferreira@ipea.gov.br>
2021-11-08 21:06:43 -0300

    Começo da atualização de scripts
    Script para atualizar base de dados diariamente com dados da curva zero

--- curva_zero_automatico.R
+++ curva_zero_automatico.R
@@ -1,12 +1,30 @@
+# Pacotes
 library(GetTDData)
 library(tidyr)
 library(rio)
+library(dplyr)
+
+# Coletando dados
 df.yield <- get.yield.curve()
+years <- data.frame(anos = seq(0.5,10,0.5), vertices = c(126, 252, 378, 504, 630, 756, 882, 1008, 1134, 1260, 1386, 1512, 1638, 1764, 1890, 2016, 2142, 2268, 2394, 2520))
 
+value.nominal <- df.yield %>% filter(type == "nominal_return") %>% select(vertices = n.biz.days, nominal = value) %>% merge(years, by = "vertices")
+value.real <- df.yield %>% filter(type == "real_return") %>% select(vertices = n.biz.days, real = value) %>% merge(years, by = "vertices")
+value.imp <- df.yield %>% filter(type == "implicit_inflation") %>% select(vertices = n.biz.days, implicita = value) %>% merge(years, by = "vertices")
+
+values <- value.nominal %>%
+  merge(value.real, by = "vertices") %>%
+  merge(value.imp, by = "vertices") %>%
+  select(anos, vertices, real, nominal, implicita)
+
+values$data <- as.character(df.yield[1,"current.date"])
+
+# Nomeando (colocando a data)
 nome <- as.character(df.yield[1,"current.date"])
-assign(nome, df.yield)
+assign(nome, values)
 
-export(get(nome), paste(nome,"xlsx",sep = "."))
+# Exportando arquivo
+rio::export(get(nome), paste(nome,"xlsx",sep = "."))
 
 # Open the scheduler: START -> All Programs -> Accesories -> System Tools -> Scheduler
 # Create a new Task
